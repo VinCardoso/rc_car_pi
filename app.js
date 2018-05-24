84a1029d0d7af6f66345d8c14ff9c2d23d3c709a
Vinicius Cardoso <viniciuscc10@gmail.com>
2018-05-24 13:46:55 +0000

    Arrumando e organizando arquivos e pastas

--- app.js
+++ app.js
@@ -1,109 +1,161 @@
-var		Gpio = require('onoff').Gpio;
-var 	keypress = require('keypress');
+// Importar Bibliotecas
+var 	express = require('express');  
+var 	app = express();  
+var 	server = require('http').createServer(app);  
+var 	io = require('socket.io')(server);
+var		pigpio = require('pigpio'),
+  		Gpio = pigpio.Gpio;
 
-		in1 = new Gpio(4, 'out'),
-		in2 = new Gpio(17, 'out'),
-		in3 = new Gpio(27, 'out'),
-		in4 = new Gpio(22, 'out');
+		
+		pigpio.configureClock(5, pigpio.CLOCK_PWM);		// Definir Clock do PWM dos motores
+
+		// Configurar Pinos
+		in1 	= new Gpio(5, 	{mode: Gpio.OUTPUT}),	// Enable 1
+		in2 	= new Gpio(6, 	{mode: Gpio.OUTPUT}),	// Enable 2
+		in3 	= new Gpio(27, 	{mode: Gpio.OUTPUT}),	// Enable 3
+		in4 	= new Gpio(22, 	{mode: Gpio.OUTPUT}),	// Enable 4
+
+		in5 	= new Gpio(23, 	{mode: Gpio.INPUT}),	// Sinal de Leitura de Velocidade do Encoder
+
+		// Set PWM
+		pwm1	= new Gpio(13,	{mode: Gpio.OUTPUT}),	// Saída do PWM Motor 1
+		pwm2	= new Gpio(17,	{mode: Gpio.OUTPUT});	// Saída do PWM Motor 2
 
 
-		function forward() {
-			in1.writeSync(1);
-			in2.writeSync(0);
-			in3.writeSync(1);
-			in4.writeSync(0);
-		}
 
-		function backward() {
-			in1.writeSync(0);
-			in2.writeSync(1);
-			in3.writeSync(0);
-			in4.writeSync(1);
-		}
 
-		function right() {
-			in1.writeSync(0);
-			in2.writeSync(1);
-			in3.writeSync(1);
-			in4.writeSync(0);
-		}
-
-		function left() {
-			in1.writeSync(1);
-			in2.writeSync(0);
-			in3.writeSync(0);
-			in4.writeSync(1);
-		}
-
+		// Funções de Alterar Enable's de acordo com posicionamento
 		function stop(){
-			in1.writeSync(1);
-			in2.writeSync(1);
-			in3.writeSync(1);
-			in4.writeSync(1);
+			in1.digitalWrite(1);
+			in2.digitalWrite(1);
+			in3.digitalWrite(1);
+			in4.digitalWrite(1);
+			console.log('Stop');
 		}
 
+		stop(); // Para carrinho antes de qualquer coisa!
+
+		function front() {
+			in1.digitalWrite(1);
+			in2.digitalWrite(0);
+			in3.digitalWrite(1);
+			in4.digitalWrite(0);
+			console.log('Front');
+		}
+
+		function back() {
+			in1.digitalWrite(0);
+			in2.digitalWrite(1);
+			in3.digitalWrite(0);
+			in4.digitalWrite(1);
+			console.log('Back');
+		}
+
+
+		
+		// Duty Cycle Inicial
+		duty_left = 0;
+		duty_right = 0;
+
+
+		total = 0;
+		p = in5.digitalRead();
+		p_last = 0;
+		cont = 0;
+
+		setInterval(function () {
+
+			pwm1.pwmWrite(duty_right);
+			pwm2.pwmWrite(duty_left);
+
+			p = in5.digitalRead();
+			//io.emit('valor',in5.digitalRead());
+			total = total + 1;
+			if(total != 100){
+				if(p == p_last){
+
+				}else{
+					if(p == 1 && p_last == 0){
+						cont = cont + 1;
+					}
+					
+					p_last = p;
+
+				}
+			}else{
+				
+				io.emit('valor',cont);
+				cont = 0;
+				total = 0;
+			}
+			
+
+
+		}, 10);
+
+
+
+app.use(express.static(__dirname));  
+server.listen(80);  
+
+min = 0;
+max = 255;
+
+
+
+
+
+// Conexão com o Controle
+io.on('connection', function(socket){
+
+	// Receber e tratar Joystick para mover carrinho
+	socket.on('joy', function(a,b,c){
+		
+		x = a;
+		y = b;
+		speed = c;
+
+		if(x>0){
+			duty_left = ((speed)*max).toFixed(0);
+			duty_right = ((speed - (speed*x))*max).toFixed(0);
+		}else if(x<0){
+			duty_left = ((speed - (speed*(-x)))*max).toFixed(0);
+			duty_right = ((speed)*max).toFixed(0);
+		}
+
+		if (y<0){
+			front();
+		}else if(y>0){
+			back();
+		} 
+
+
+	});
+
+	// Receber Soltou Joystick
+	socket.on('unpress', function(a,b){
 		stop();
+	});
 
-		/*
-		var frente = setTimeout(	forward, 	10);
-		var parar = setTimeout(		stop,		2000);
-		var tras = setTimeout(		backward, 	3000);
-		var parar2 = setTimeout(	stop,		4000);
-		var tras = setTimeout(		right, 		7000);
-		var parar = setTimeout(		stop,		8000);
-		var tras = setTimeout(		left, 		10000);
-		var parar = setTimeout(		stop,		11000);
-		*/
+	// Receber Trechos
+	socket.on('data', function(a){
+		console.log(a);
 
+		var trechos = a;
 
+		while(trechos != 0){
+			led.digitalWrite(1);
+			setTimeout(function(){
+				led.digitalWrite(0);
+			},500);
+			setTimeout(function(){
+				trechos = trechos -1;
+			},500);
+	
+		}
+	});
 
-// make `process.stdin` begin emitting "keypress" events
-keypress(process.stdin);
-
-// listen for the "keypress" event
-var stopmotor;
-process.stdin.on('keypress', function (ch, key) {
-    //console.log('got "keypress"', key);
-
-    if (key && key.name == 'up') {
-        console.log("A");
-        if (stopmotor)
-            clearTimeout(stopmotor);
-        forward();
-    } else if (key && key.name == 'down') {
-        console.log("V");
-        if (stopmotor)
-            clearTimeout(stopmotor);
-        backward();
-    } else if (key && key.name == 'left') {
-        console.log("<");
-        if (stopmotor)
-            clearTimeout(stopmotor);
-        left();
-    } else if (key && key.name == 'right') {
-        console.log(">");
-        if (stopmotor)
-            clearTimeout(stopmotor);
-        right();
-    } else if (key && key.ctrl && key.name == 'c') {
-        process.stdin.stop();
-    }
-
-    stopmotor = setTimeout(function () {
-        stop();
-    }, 100);
 });
-
-process.stdin.setRawMode(true);
-process.stdin.resume();
-
-process.on('SIGINT', function () {
-    console.log("Reversed all pin to 0");
-    pin_reset();
-    process.exit();
-});
-
-
 
 
 
